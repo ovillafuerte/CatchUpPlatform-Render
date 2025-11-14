a1b23eb938b456fa2ec82a530907aed5fd6e00bc
unknown <oscarvillafuerte@gmail.com>
2025-11-13 19:46:37 -0500

    subiendo actualizando el puerto 3

--- CatchUpPlatform.API/Program.cs
+++ CatchUpPlatform.API/Program.cs
@@ -108,4 +108,9 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// Forzar escucha en el puerto que pasa Render (o 8080 por defecto)
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Clear();
+app.Urls.Add($"http://*:{port}");
+
 app.Run();
