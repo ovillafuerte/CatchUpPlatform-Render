e2c0e7fda4835e96e99170ecdd9702b567968f4b
unknown <oscarvillafuerte@gmail.com>
2025-11-15 00:37:52 -0500

    actulizando program 2

--- CatchUpPlatform.API/Program.cs
+++ CatchUpPlatform.API/Program.cs
@@ -8,6 +8,8 @@ using CatchUpPlatform.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
 using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 
 
 var builder = WebApplication.CreateBuilder(args);
